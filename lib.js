9ceb3af34534a604274f1ce76bd79d6e5e872639
nicodechal <nicodechal@outlook.com>
2019-07-19 18:55:46 +0800

    add tokenizer.js

--- lib.js
+++ lib.js
@@ -1,9 +1,11 @@
 import Heap from "./lib/heap";
 import permute from "./lib/permute";
 import UnionFind from "./lib/union-find";
+import Tokenizer from "./lib/tokenizer";
 
 export {
   Heap,
   permute,
-  UnionFind
+  UnionFind,
+  Tokenizer
 }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { Heap, permute, UnionFind } from "./lib"
+import { Heap, permute, UnionFind, Tokenizer } from "./lib"
 
 function testHeap() {
   const h = new Heap((a, b) => a < b);
@@ -19,12 +19,46 @@ function testPermutation() {
   console.log(permute([1,2,3]))
 }
 
-testPermutation()
-testHeap()
-const uf = new UnionFind(10);
-console.log(uf.join(0,1));
-console.log(uf.join(1,2));
-console.log(uf.join(2,3));
-console.log(uf.join(3,4));
-console.log(uf.p);
-console.log(uf.find(2));
+function testUnionFind() {
+  const uf = new UnionFind(10);
+  console.log(uf.join(0,1));
+  console.log(uf.join(1,2));
+  console.log(uf.join(2,3));
+  console.log(uf.join(3,4));
+  console.log(uf.p);
+  console.log(uf.find(2));
+}
+
+function testTokenizer() {
+  const tz = new Tokenizer([{
+    cb () {
+      const tk = this.nextToken();
+      const matched = tk === '{' || tk === '}' || tk === ',';
+      if (!matched) return false;
+      this.index++;
+      this.result.push(tk);
+      return true;
+    }
+  }, {
+    cb () {
+      let res = "";
+      while (!this.end()) {
+        const tk = this.nextToken().charCodeAt(0);
+        if (tk >= 'a'.charCodeAt(0) && tk <= 'z'.charCodeAt(0)) {
+          res += this.nextToken();
+          this.index++;
+        } else break;
+      }
+      if (res === "") return false;
+      this.result.push(res);
+      return true;
+    }
+  }], "{{a,z},a{b,c},{ab,z}}")
+
+  console.log(tz.tokens());
+}
+
+// testPermutation()
+// testHeap()
+// testUnionFind()
+// testTokenizer()
