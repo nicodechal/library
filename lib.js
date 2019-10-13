97c11c0e40e4751b0b28266301d48dcc16251d3d
nicodechal <nicodechal@outlook.com>
2019-10-13 22:48:18 +0800

    automate the generation of lib.js

--- lib.js
+++ lib.js
@@ -1,14 +1,24 @@
-import Permute from "./lib/algo/permute";
-import Kmp from "./lib/algo/kmp";
-import Tokenizer from "./lib/algo/tokenizer";
-import UnionFind from "./lib/data-structure/union-find";
-import Heap from "./lib/data-structure/heap";
+const fs = require('fs');
+const path = require('path');
 
+const BASE = "./lib";
+const modules = {};
 
-export {
-  Heap,
-  Permute,
-  Kmp,
-  UnionFind,
-  Tokenizer
+(function importModules(dirName = BASE) {
+  const baseDir = fs.readdirSync(dirName, { withFileTypes: true });
+  for (const p of baseDir) {
+    if (p.isDirectory()) {
+      importModules(`${dirName}/${p.name}`);
+    }
+    if (p.isFile()) {
+      const mName = kebab2BigCamel(path.parse(p.name).name);
+      modules[mName] = require(`${dirName}/${p.name}`).default;
+    }
+  }
+})();
+
+function kebab2BigCamel (str) {
+  return str.split('-').map(v => v[0].toUpperCase() + v.slice(1)).join('');
 }
+
+module.exports = modules;
